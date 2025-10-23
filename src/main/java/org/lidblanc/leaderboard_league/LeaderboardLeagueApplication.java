a03ae38ec1094b3a5dd6372eeb7a8cba7b240cc5
Zachary Kocienda <zacharykocienda@gmail.com>
2025-10-23 12:39:30 -0700

    Initial commit - again....

---
+++ src/main/java/org/lidblanc/leaderboard_league/LeaderboardLeagueApplication.java
@@ -0,0 +1,13 @@
+package org.lidblanc.leaderboard_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardLeagueApplication.class, args);
+	}
+
+}
